38cf17310d2ec031b54efaf7414ef215afef3683
dreamt <dreamt96@outlook.com>
2022-02-18 16:18:45 +0800

    add dynamic load resources

--- src/components/blog/Blog.vue
+++ src/components/blog/Blog.vue
@@ -1,16 +1,20 @@
 <template>
   123
-  {{txt}}
+  {{ txt }}
 </template>
 <script lang="ts">
 import {Vue} from "vue-class-component";
-import markdown from "raw-loader!./first-blog.md";
+// import markdown from "raw-loader!../../../writing/first-blog.md";
 
 export default class Blog extends Vue {
-  txt= "string";
+  txt = "string";
 
-  mounted(){
-    this.txt = markdown
+  mounted() {
+    setInterval(() => {
+      let num = Date.now() % 2 + 1;
+      let raw = require("raw-loader!../../../writing/" + num + ".md");
+      this.txt = JSON.stringify(raw);
+    }, 500)
   }
 }
 </script>
