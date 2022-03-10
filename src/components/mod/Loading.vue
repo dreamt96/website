9974c3c0553721db864e7d81f8f19df9d1f09781
dreamt <dreamt96@outlook.com>
2022-03-10 19:38:15 +0800

    1

---
+++ src/components/mod/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="root">
+<!--todo-->
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Loading"
+})
+</script>
+
+<style scoped lang="scss">
+.root {
+}
+</style>
+
